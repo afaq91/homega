6a6ee368fae71028cb7d8b1a1436b6263c1cfcb8
Afaq <afaqchaudhary189@gmail.com>
2024-06-22 17:22:56 +0400

    wewe

--- src/components/homes/home-six/HeroBanner.tsx
+++ src/components/homes/home-six/HeroBanner.tsx
@@ -15,7 +15,7 @@ const HeroBanner = () => {
                         <h1 className="hero-heading">Unlock Your Real Estate<br></br> Potential with<br></br>Homega</h1>
                      </div>
                      <div className="ps-xxl-1 pt-35 wow fadeInRight">
-                        <p className="color-dark sub-heading mb-40 lg-mb-20">Over 745,000 listings, apartments, lots and<br></br>plots available now!</p>
+                        <p className="color-dark sub-heading mb-40 lg-mb-20">Over 100+ listings, apartments, lots and<br></br>plots available now!</p>
                         <div className="d-flex flex-wrap justify-content-between align-items-start">
                            <div className="d-flex flex-column justify-content-center align-items-start mt-0">
                            <Link href="/contact" className="btn-five rounded-0 md mt-20"><span>Contact us</span> <i className="bi bi-arrow-up-right"></i></Link>  
