efa605faa38485ad692d636c2d0389c912402bed
Afaq <afaqchaudhary189@gmail.com>
2024-06-25 08:50:36 +0400

    block section chnage

--- src/components/homes/home-four/BLockFeatureOne.tsx
+++ src/components/homes/home-four/BLockFeatureOne.tsx
@@ -1,3 +1,4 @@
+'use-client'
 import FeatureOne from "@/components/common/FeatureOne"
 
 const BLockFeatureOne = () => {
@@ -8,19 +9,49 @@ const BLockFeatureOne = () => {
                <div className="row">
                   <div className="col-xxl-6 col-xl-7 col-lg-8">
                      <div className="title-one mb-30 lg-mb-20 wow fadeInUp">
-                        <h3>We’r here to help you find properties.</h3>
-                        <p className="fs-22">Over 100+ listings of apartments, villas and plots available.</p>
+                        <h3>About Us</h3>
+                        <p className="fs-22">Welcome to Homega Real Estate, your premier partner for visionary real estate projects in the vibrant UAE market. Founded on a commitment to integrity, innovation, and excellence, we specialize in creating distinctive residential and commercial developments that redefine urban living.</p>
                      </div>
+
+<div className="d-flex customblockicons">
+
+<div className="customiconcontainer">
+ <div className="iconcustom">  
+<img alt="" loading="lazy" width="27" height="25" decoding="async" data-nimg="1" className="lazy-img" src="/_next/static/media/icon_35.944d9cf9.svg"></img>
+</div>
+<h5 className="mt-35 mb-20">Buy</h5>
+</div>
+
+
+<div className="customiconcontainer">
+<div className="iconcustom">  
+<img alt="" loading="lazy" width="29" height="27" decoding="async" data-nimg="1" className="lazy-img" src="/_next/static/media/icon_36.09ff6d3d.svg"></img>
+</div>
+<h5 className="mt-35 mb-20">Rent</h5>
+</div>
+
+
+<div className="customiconcontainer">
+<div className="iconcustom">  
+<img alt="" loading="lazy" width="28" height="27" decoding="async" data-nimg="1" className="lazy-img" src="/_next/static/media/icon_37.f6e71d7d.svg"></img>
+</div>
+<h5 className="mt-35 mb-20">Sell</h5>
+</div>
+
+
+</div>
+
                   </div>
                </div>
 
-               <div className="row gx-xxl-5">
+               {/* <div className="row gx-xxl-5">
                   <FeatureOne />
-               </div>
+               </div> */}
             </div>
          </div>
       </div>
    )
 }
+
 
 export default BLockFeatureOne
